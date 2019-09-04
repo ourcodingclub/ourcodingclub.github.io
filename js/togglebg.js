25bb89cbf9cdfd2c576779a7b8bc5b8e16e8e4d0
sandra-ab <sandra-ab@github.com>
2019-09-04 15:52:05 +0100

    new test toggle

--- js/togglebg.js
+++ js/togglebg.js
@@ -1,20 +1,9 @@
 // toggle button to turn background images on and off
 
-var id=["stream1tuts", "stream2tuts","stream3tuts", "challenge1go", "challenge2go", "challenge3go"];
-var i;
-
-function togglebg(){
-   for (i = 0; i < id.length; i++) { //start of loop 2
-
-   // toggle the bg depending on current state
-    if (document.getElementById(id[i]).class=='feature') {
-      // bg is visible. hide it
-      document.getElementById(id[i]).class='nobg';
-    }
-    else {
-      // bg is invisible. show it
-      document.getElementById(id[i]).class ='feature';
-    }
-}//end of loop 2
+window.onload = function togglebg(){
+  if (document.getElementById('skills').classList.contains('nobg') {
+document.getElementById('skills').classList.remove('nobg');
+} else {
+document.getElementById('skills').classList.add('nobg');   
 }
-)
+}
