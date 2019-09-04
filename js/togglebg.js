289e34220a2f785d85a162cb2d394c49205be100
sandra-ab <sandra-ab@github.com>
2019-09-04 16:09:57 +0100

    new test toggle

--- js/togglebg.js
+++ js/togglebg.js
@@ -1,9 +1,13 @@
 // toggle button to turn background images on and off
 
-window.onload = function togglebg(){
+function togglebg(){
   if (document.getElementById('skills').classList.contains('nobg') {
 document.getElementById('skills').classList.remove('nobg');
 } else {
 document.getElementById('skills').classList.add('nobg');   
 }
+}
+
+function toggle2() {
+$('#skills').toggleClass('nobg')
 }
