47de716f45946c043c2c21a31d949160dfca47bb
sandra-ab <sandra-ab@github.com>
2019-09-05 14:24:18 +0100

    Testing a copy-paste function

---
+++ js/copypaste.js
@@ -0,0 +1,16 @@
+// Copy and paste function
+
+$(document).ready(function(){
+   $("a[name=copy_pre]").click(function() {
+      var id = $(this).attr('id').slice(1); //always have letter A as prefix to link ID
+      var el = document.getElementById(id);
+      var range = document.createRange();
+      range.selectNodeContents(el);
+      var sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+      document.execCommand('copy');
+      alert("Contents copied to clipboard.");
+      return false;
+   });
+});
