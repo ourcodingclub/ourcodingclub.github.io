09adaca38d697ebe4554e45f0e3861c9dc5de47b
sandra-ab <sandra-ab@github.com>
2019-09-05 12:31:24 +0100

    almost there; classes changing but not img

--- js/togglebg.js
+++ js/togglebg.js
@@ -12,6 +12,18 @@ function toggle2(id) {
 $(id).toggleClass('nobg');
 }
 
+function toggle3(id) {
+   if ($(id).hasClass('feature')) {
+   $(id).removeClass('feature');
+   $(id).addClass('nobg');
+   }
+   else {
+   $(id).removeClass('nobg');
+   $(id).addClass('feature');   
+   }
+}
+
+$('#MyElement').removeClass('MyClass');
 
 $('.btn-switch').click(function toggle(id) {
 $(id).toggleClass('nobg');
