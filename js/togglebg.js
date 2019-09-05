543c1273b18747245425bb52ffc21f669cb93f7d
sandra-ab <sandra-ab@github.com>
2019-09-05 13:07:12 +0100

    Cleaned up code and added button to workflow section

--- js/togglebg.js
+++ js/togglebg.js
@@ -1,30 +1,5 @@
 // toggle button to turn background images on and off
 
-function togglebg(id){
-  if (document.getElementById(id).classList.contains('nobg')){
-document.getElementById(id).classList.remove('nobg');
-} else {
-document.getElementById(id).classList.add('nobg');   
-}
-}
-
-function toggle2(id) {
+function toggle(id) {
 $(id).toggleClass('nobg');
 }
-
-function toggle3(id) {
-   if ($(id).hasClass('feature')) {
-   $(id).removeClass('feature');
-   $(id).addClass('nobg');
-   }
-   else {
-   $(id).removeClass('nobg');
-   $(id).addClass('feature');   
-   }
-}
-
-$('#MyElement').removeClass('MyClass');
-
-$('.btn-switch').click(function toggle(id) {
-$(id).toggleClass('nobg');
-});
